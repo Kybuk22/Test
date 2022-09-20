ff06a5d7958aba0b270ddccefb3a4690c032b83b
Kubyk <93663147+Kybuk22@users.noreply.github.com>
2022-09-20 16:06:25 +0200

    Add files via upload

---
+++ PESEL/base.sql
@@ -0,0 +1,42 @@
+USE baza
+
+Create table lokale (
+id Int IDENTITY(1,1), 
+nazwa VARCHAR(100) ,
+miasto VARCHAR(100) ,
+ulica VARCHAR(100),
+number Int ,
+)
+Create table dania (
+id Int IDENTITY(1,1), 
+typ Int,
+nazwa VARCHAR(100),
+cena Int 
+)
+Create table rezerwacje (
+id Int IDENTITY(1,1), 
+nr_stolika Int,
+data_rez Date,
+licza_osob Int ,
+telefon VARCHAR(100)
+)
+Create table pracownicy(
+id Int IDENTITY(1,1), 
+imie VARCHAR(100),
+nazwa VARCHAR(100),
+stanowisko Int,
+)
+
+Insert into rezerwacje 
+Values(
+1,'2017-07-04',4,'111222333')
+
+Select * from dania
+Order by data_rez
+
+Select nazwa from dania
+Where cena <15
+
+Update pracownicy
+Set stanowiska='barman'
+Where stanowiska='kelner'
