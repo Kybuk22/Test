b436d91c97850b7b51b7a964e2c0355561434c31
Kubyk <93663147+Kybuk22@users.noreply.github.com>
2022-09-20 16:14:30 +0200

    Update base.sql

--- PESEL/base.sql
+++ PESEL/base.sql
@@ -7,12 +7,14 @@ miasto VARCHAR(100) ,
 ulica VARCHAR(100),
 number Int ,
 )
+GO
 Create table dania (
 id Int IDENTITY(1,1), 
 typ Int,
 nazwa VARCHAR(100),
 cena Int 
 )
+GO
 Create table rezerwacje (
 id Int IDENTITY(1,1), 
 nr_stolika Int,
@@ -20,17 +22,18 @@ data_rez Date,
 licza_osob Int ,
 telefon VARCHAR(100)
 )
+GO
 Create table pracownicy(
 id Int IDENTITY(1,1), 
 imie VARCHAR(100),
 nazwa VARCHAR(100),
 stanowisko Int,
 )
-
+GO
 Insert into rezerwacje 
 Values(
 1,'2017-07-04',4,'111222333')
-
+GO
 Select * from dania
 Order by data_rez
 
