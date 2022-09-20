8674bb9a7eb5959a8abfe6f658f345397ec88f3c
Kubyk <93663147+Kybuk22@users.noreply.github.com>
2022-09-20 16:23:40 +0200

    Update base.sql

--- PESEL/base.sql
+++ PESEL/base.sql
@@ -1,39 +1,36 @@
 USE baza
 
 Create table lokale (
-id Int IDENTITY(1,1), 
+id Int unsigned auto_increment primary key, 
 nazwa VARCHAR(100) ,
 miasto VARCHAR(100) ,
 ulica VARCHAR(100),
 number Int ,
 )
-GO
 Create table dania (
-id Int IDENTITY(1,1), 
+id Int unsigned auto_increment primary key, 
 typ Int,
 nazwa VARCHAR(100),
 cena Int 
 )
-GO
 Create table rezerwacje (
-id Int IDENTITY(1,1), 
+id Int unsigned auto_increment primary key, 
 nr_stolika Int,
 data_rez Date,
 licza_osob Int ,
 telefon VARCHAR(100)
 )
-GO
 Create table pracownicy(
-id Int IDENTITY(1,1), 
+id Int unsigned auto_increment primary key, 
 imie VARCHAR(100),
 nazwa VARCHAR(100),
 stanowisko Int,
 )
-GO
+
 Insert into rezerwacje 
 Values(
 1,'2017-07-04',4,'111222333')
-GO
+
 Select * from dania
 Order by data_rez
 
