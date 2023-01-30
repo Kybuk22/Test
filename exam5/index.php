9d898f357464b800caa138a4c82da2fa6c8b091c
Kubyk <93663147+Kybuk22@users.noreply.github.com>
2023-01-30 18:53:54 +0100

    Add files via upload

---
+++ exam5/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <html lang="pl"> 
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>malarz</title>
+  </head>
+  <body class="body">
+    <div class="banner">
+      <h1><a href="index.html">Koszty farby</a></h1>
+    </div>
+    <div class="panels">
+    <div class="left-panel">
+      <h3>Mieszamy czy wyceniamy?</h3>
+      <a href="mieszamy.php">Mieszamy</a>
+      <br>
+      <a href="cena.php">Wyceniamy</a>
+    </div>
+    <div class="right-panel">
+      <p>Strona poświęcona malarstwu:)</p>
+      <img src="obraz.jpg" alt="abstrakcja">
+    </div>
+</div>
+    <footer>
+      <p>MALARZ</p>
+      <p>Stronę opracował:</p>
+    </footer>
+  </body>
+</html>
---
+++ exam5/mieszamy.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="styl.css">
+  </head>
+  <body>
+    <div class="right-panel">
+        <p>Strona w trakcie budowy</p>
+      </div>
+  </body>
+</html>
